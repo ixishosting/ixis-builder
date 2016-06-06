61aea91a6aecbff08d6d7def3987657f0eb07ea1
michael austin <ma@ixis.co.uk>
2016-06-06 16:32:21 +0100

    debug

--- start.sh
+++ start.sh
@@ -10,7 +10,5 @@ printenv
 echo $GIT_BUILD_URL
 
 
-ssh-keyscan gogs.zerofucks.co.uk >> ~/.ssh/known_hosts
-
 ### clone build repo ###
 GIT_SSH_COMMAND='ssh -i /keys/id_gogs -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' git clone $GIT_BUILD_URL /tmp/build
