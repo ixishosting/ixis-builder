69c1507cb72b5fc66d68a02049163d5aa747ce78
michael austin <ma@ixis.co.uk>
2016-06-06 16:27:35 +0100

    debug

--- start.sh
+++ start.sh
@@ -5,11 +5,9 @@
 ###
 
 ### setup ssh keys ###
-export GIT_SSH_COMMAND="ssh -i /keys/id_gogs"
-
 printenv
 
 echo $GIT_BUILD_URL
 
 ### clone build repo ###
-git clone $GIT_BUILD_URL /tmp/build
+GIT_SSH_COMMAND='ssh -i /keys/id_gogs -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' git clone $GIT_BUILD_URL /tmp/build
